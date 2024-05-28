008e5cfd80c6da461139525ffc2fcc3cd3d78087
Juandasalazarg <141048830+Juandasalazarg@users.noreply.github.com>
2024-05-28 16:58:47 -0500

    Update controlador_usuario.py

--- app/controlador_usuario.py
+++ app/controlador_usuario.py
@@ -1,45 +1,24 @@
-"""
-Módulo controlador_usuario.py
+from usuario import Usuarios
 
-Este módulo contiene funciones para el manejo de usuarios.
-"""
+usuariox = Usuarios()
 
-from usuario import *
-
-def agregar_usuario(self, nombre, usuario, contrasena, email):
-        try:
-            cursor = self.connection.cursor()
-            cursor.execute("INSERT INTO Usuario (nombre, usuario, contraseña, email) VALUES (%s, %s, %s, %s)",
-                           (nombre, usuario, contrasena, email))
-            self.connection.commit()
-        finally:
-            cursor.close()
-
-def borrar_usuario(self, usuario, contrasena):
+def agregar_usuario(nombre, usuario, contrasena, email):
     try:
-        cursor = self.connection.cursor()
-        cursor.execute("DELETE FROM Usuario WHERE usuario = %s AND contraseña = %s", (usuario, contrasena))
-        self.connection.commit()
-    finally:
-        cursor.close()
+        usuariox.agregar_usuario(nombre, usuario, contrasena, email)
+        print("Usuario agregado correctamente")
+    except Exception as e:
+        print(f"Error al agregar usuario: {e}")
 
-def actualizar_usuario(self, usuario, nombre, nuevo_usuario, nueva_contrasena, nuevo_email):
+def consultar_usuario(usuario):
     try:
-        cursor = self.connection.cursor()
-        cursor.execute("UPDATE Usuario SET nombre = %s, usuario = %s, contraseña = %s, email = %s WHERE usuario = %s",
-                        (nombre, nuevo_usuario, nueva_contrasena, nuevo_email, usuario))
-        self.connection.commit()
-    finally:
-        cursor.close()
-
-def consultar_usuario(self, usuario):
-    try:
-        cursor = self.connection.cursor()
-        cursor.execute("SELECT * FROM Usuario WHERE usuario = %s", (usuario,))
-        user_data = cursor.fetchone()
+        user_data = usuariox.consultar_usuario(usuario)
         return user_data
-    finally:
-        cursor.close()
+    except Exception as e:
+        print(f"Error al consultar usuario: {e}")
 
-def __del__(self):
-    self.connection.close()
+def actualizar_usuario(usuario, nombre, nuevo_usuario, nueva_contrasena, nuevo_email):
+    try:
+        usuariox.actualizar_usuario(usuario, nombre, nuevo_usuario, nueva_contrasena, nuevo_email)
+        print("Usuario actualizado correctamente")
+    except Exception as e:
+        print(f"Error al actualizar usuario: {e}")
